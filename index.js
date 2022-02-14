31624e521560c319843964384ce4091ab9d30bac
multimediajane <66893535+multimediajane@users.noreply.github.com>
2022-02-14 13:55:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -29,13 +29,28 @@ let todayDate = document.querySelector(".todayDate");
 
 todayDate.innerHTML = ` ${day},  ${date}/${month}/${year} ${hour}:${minutes} `;
 
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-city-input");
-
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${searchInput.value}`;
-}
-
+let searchButton = document.querySelector("#searchButton");
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let currentCity = document.querySelector("h1");
+  currentCity.innerHTML = `${city}`;
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = `${temperature}`;
+  let description = response.data.weather[0].main;
+  let currentDescription = document.querySelector("#description");
+  currentDescription.innerHTML = `${description}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("h1");
+  let searchInput = document.querySelector("#search-city-input");
+  h1.innerHTML = `${searchInput.value}`;
+  let apiKey = "7408fdf253ddebead7e68be64b518024";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
